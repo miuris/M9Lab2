62ee0431988541e42898fccf8847f54e4c95905e
Miuris.G <miuris.gutierrez@gmail.com>
2025-07-29 00:42:30 -0400

    TextInput update

--- character-counter/src/components/CharacterCounter/CharacterCounter.tsx
+++ character-counter/src/components/CharacterCounter/CharacterCounter.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 export default function CharacterCounter() {
   return (
     <div>
-      
+      <h3>Counter : {state.count}</h3>
+
+      <button onClick={decrement}>Decrease</button>
+      <button onClick={increment}>Increase</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
--- character-counter/src/components/TextInput/TextInput.tsx
+++ character-counter/src/components/TextInput/TextInput.tsx
@@ -1,12 +1,21 @@
-import { TextInputProps } from "../types";
-import React from 'react'
-
-
-export default function TextInput({}) {
+import React from 'react';
+import { TextInputProps } from '../../types';
+import './TextInput.css';
  
-    return (
-    <div>
-      How many pieces do you wish?
+export const TextInput: React.FC<TextInputProps> = ({
+  onTextChange,
+  placeholder = 'Start typing...',
+  initialValue = ''
+}) => {
+  return (
+    <div className="text-input-container">
+      <textarea
+        className="text-input-textarea"
+        placeholder={placeholder}
+        defaultValue={initialValue}
+        onChange={(e) => onTextChange(e.target.value)}
+        rows={6}
+      />
     </div>
-  )
-}
+  );
+};
